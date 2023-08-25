7a5363dfc8fa2dc82c75e3ae18eaf1638d255ca5
anthonytamzil <anthony.tamzil@gmail.com>
2023-08-25 21:17:38 +0800

    Add job description

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
+import { useState } from 'react';
 import './App.css'
 import JobDescription from './components/JobDescription';
 import JobListingCard from './components/JobListingCard';
 import { jobs } from './constants';
 
 export default function App() {
+  const [selectedJob, changeSelection] = useState(0);
+
   return (
     <>
       <div className="w-screen lg:w-full flex-row flex">
         <div className="flex-col">
           {jobs?.map((job) => (
-            <JobListingCard 
-              employer={job.employer} 
-              jobTitle={job.jobTitle} 
-              postingTime={job.postingTime}
-              location={job.location}
-              techStack={job.techStack}
-              field={job.field}
-              rating={job.rating}
-              logo={job.logo}
-            />
+            <div onClick={() => changeSelection(jobs.indexOf(job))}>
+              <JobListingCard 
+                employer={job.employer} 
+                jobTitle={job.jobTitle} 
+                postingTime={job.postingTime}
+                location={job.location}
+                techStack={job.techStack}
+                field={job.field}
+                rating={job.rating}
+                logo={job.logo}
+              />
+            </div>
           ))}
         </div>
-        <JobDescription />
+        <JobDescription 
+          employer={jobs[selectedJob].employer}
+          jobTitle={jobs[selectedJob].jobTitle}
+          logo={jobs[selectedJob].logo}
+        />
       </div>
     </>
   )
--- src/components/JobDescription.tsx
+++ src/components/JobDescription.tsx
@@ -1,7 +1,23 @@
-export default function JobDescription() {
+type JobDescriptionProps = {
+  employer: string;
+  jobTitle: string;
+  logo: string;
+}
+
+export default function JobDescription({
+  employer,
+  jobTitle,
+  logo
+} : JobDescriptionProps) {
   return (
-    <div className='text-black bg-white hidden lg:flex w-[683px] p-[5px]'>
-      hi
+    <div className="text-black hidden lg:flex w-[683px] p-[5px] overflow-auto ">
+      <div className="p-[5px] bg-white w-[683px] rounded-lg h-[100vh] fixed">
+        <img src={logo} className="w-20 h-20 mx-auto mt-12"/>
+        <p className=" text-lg font-semibold mt-4">
+          {employer}
+        </p>
+        {jobTitle}
+      </div>
     </div>
   )
 }
