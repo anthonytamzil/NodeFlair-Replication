fccfbc56a659bd86f5db65709eadb92d57249344
anthonytamzil <anthony.tamzil@gmail.com>
2023-08-26 00:49:34 +0800

    Fix UI bugs

--- src/components/JobListingCard.tsx
+++ src/components/JobListingCard.tsx
@@ -26,10 +26,10 @@ export default function JobListingCard({
   est,
 }: JobListingCardProps) {
   return (
-    <div className="p-[5px] w-screen lg:w-fit">
+    <div className="p-[10px] w-screen lg:w-fit">
       <div className="h-fit w-full lg:w-[417px] bg-white border-[#E1E0DD] border p-[5px] rounded-lg hover:shadow cursor-pointer hover:delay-75 focus:border-4 focus:border-[#1FC76A]">
-        <div className="px-5 pt-4 w-full h-fit">
-          <div className="flex-row flex">
+        <div className="px-5 pt-4 w-full h-fit flex">
+          <div className="flex-row flex max-[440px]:flex-col">
             <div className="min-w-[55px]">
               <img className="rounded-[4px] font-[14px] w-[45px] h-[45px]" src={logo}/>
             </div>
@@ -39,27 +39,28 @@ export default function JobListingCard({
                 <span className="ml-3 text-[14px]">{rating} {rating === null ? "" : "★"}</span>
               </p>
               <p className="leading-6 text-lg font-bold text-left">{jobTitle}</p>
-            </div>
-            <div className="ml-auto text-right">
-              <div className="bg-[#DDF7E9] py-[5px] px-[8px] rounded-md">
-                <p className="text-[#1FC793] font-semibold text-[15px]">{field}</p>
+              <div className="flex-row flex text-[#838383]">
+                <span className="text-[#1fc76a] font-bold">
+                  {postingTime} minutes ago
+                </span>
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marker-alt" className="w-[10.5px] h-[14px] ml-4 mt-[6px]" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg>
+                <div>
+                  &nbsp;{location}
+                </div>
+              </div>
+              <div className="text-black text-left text-[15px] mt-[6px] font-medium">
+                {salary} 
+                <span className={est ? "ml-2 font-normal bg-[#ececec] px-2 py-1 rounded-lg" : "hidden"}>
+                  EST
+                </span>
               </div>
             </div>
+            
           </div>
-          <div className="text-left flex-row flex text-[#838383] mt-[1px]">
-            <span className="text-[#1fc76a] pl-[55px] font-bold">
-              {postingTime} minutes ago
-            </span>
-            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marker-alt" className="w-[10.5px] h-[14px] ml-4 mt-[6px]" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg>
-            <div>
-              &nbsp;{location}
+          <div className="ml-auto text-right">
+            <div className="bg-[#DDF7E9] py-[5px] px-[8px] rounded-md">
+              <p className="text-[#1FC793] font-semibold text-[15px]">{field}</p>
             </div>
-          </div>
-          <div className="text-black text-left pl-[55px] text-[14px] mt-[6px] font-medium">
-            {salary} 
-            <span className={est ? "ml-2 font-normal bg-[#ececec] px-2 py-1 rounded-lg" : "hidden"}>
-              EST
-            </span>
           </div>
         </div>
         
